aeed98ecc8cd357a3c051866bf4b499a65900cfa
Peng <passren9099@hotmail.com>
2014-12-30 15:06:52 +0800

    Update DataSource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -18,7 +18,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:file:database/minicrmDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             
 //            dbCreate = "update"
