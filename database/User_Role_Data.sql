d49c97fdabb985c105af3e83c164b8161975ca18
peng <passren9099@hotmail.com>
2015-01-03 11:14:22 +0800

    Add init data for user

---
+++ database/User_Role_Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `role` (`authority`, `description`) VALUES ('ROLE_ADMIN', '管理员');
+INSERT INTO `role` (`authority`, `description`) VALUES ('ROLE_SALES', '销售人员');
+INSERT INTO `role` (`authority`, `description`) VALUES ('ROLE_SALES_MANAGER', '销售经理');
+
+INSERT INTO `user`
+(`version`,`password_expired`,`account_expired`,`account_locked`,`enabled`,
+`created_date`,`last_updated_date`,`password`,`realname`,`username`)
+VALUES
+(0,0,0,0,1,now(),now(),'$2a$10$mkbTvcj8lGCBj1D1wXIkm.MLtvNklsRP35kPBI7F0YtDOLdnSyN.C','管理员','admin');
+
+INSERT INTO `user_role` (`role_id`, `user_id`) VALUES (1, 1);
