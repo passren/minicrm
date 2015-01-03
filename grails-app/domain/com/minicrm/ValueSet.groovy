8f8f2466277ea463de1f3313e67efc724fa0c148
peng <passren9099@hotmail.com>
2015-01-03 11:13:07 +0800

    Fix issue on ValueSet Domain Class

--- grails-app/domain/com/minicrm/ValueSet.groovy
+++ grails-app/domain/com/minicrm/ValueSet.groovy
@@ -16,8 +16,8 @@ class ValueSet {
 
     static constraints = {
         id composite: ['category', 'code']
-        category blank: false, maxSize: 20
-        code blank: false, maxSize: 30
+        category blank: false, maxSize: 50
+        code blank: false, maxSize: 50
         code1 nullable: true, maxSize: 50
         code2 nullable: true, maxSize: 50
     }
