2073f6d6af48da650119b751ff10ed9fb9d47d15
peng <passren9099@hotmail.com>
2015-01-03 10:34:31 +0800

    add test/prod datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -8,7 +8,7 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+    //    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
@@ -18,70 +18,64 @@ hibernate {
 environments {
     development {
         dataSource {
-//            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-//            url = "jdbc:h2:file:database/minicrmDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //            url = "jdbc:h2:file:database/minicrmDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             
             dbCreate = "update"
             pooled = true
-			url = "jdbc:mysql://127.0.0.1:3306/minicrm-dev"
-			driverClassName = "com.mysql.jdbc.Driver"
-			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-			username = "minicrm-dev"
-			password = "minicrm-dev"
-			properties {
-				maxActive = 50
-				maxIdle = 25
-				minIdle = 5
-				initialSize = 5
-				minEvictableIdleTimeMillis = 60000
-				timeBetweenEvictionRunsMillis = 60000
-				maxWait = 10000
-			}
+            url = "jdbc:mysql://127.0.0.1:3306/minicrm-dev"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            username = "minicrm-dev"
+            password = "minicrm-dev"
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
+            }
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-//          pooled = true
-//			url = "jdbc:mysql://127.0.0.1:3306/minicrm-dev"
-//			driverClassName = "com.mysql.jdbc.Driver"
-//			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-//			username = "minicrm-dev"
-//			password = "minicrm-dev"
-//			properties {
-//				maxActive = 50
-//				maxIdle = 25
-//				minIdle = 5
-//				initialSize = 5
-//				minEvictableIdleTimeMillis = 60000
-//				timeBetweenEvictionRunsMillis = 60000
-//				maxWait = 10000
-//			}
+            dbCreate = "validate"
+            pooled = true
+            url = "jdbc:mysql://127.0.0.1:3306/minicrm-test"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            username = "minicrm-test"
+            password = "minicrm-test"
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
+            }
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "validate"
+            pooled = true
+            url = "jdbc:mysql://127.0.0.1:3306/minicrm"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            username = "minicrm"
+            password = "minicrm"
             properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
             }
         }
     }
